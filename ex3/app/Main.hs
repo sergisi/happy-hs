d804890cb83be1e65eff3f1947b75f8616aaeb4e
Sergi Simón Balcells <sergi.simon@ce.eurecat.org>
2021-04-22 21:28:49 +0200

    It's finished but it needs testing.

--- ex3/app/Main.hs
+++ ex3/app/Main.hs
@@ -8,4 +8,8 @@ main :: IO ()
 main =
   do
     s <- getContents
-    print $ runAlex s $ fmap (fmap show . reverse) calc
+    print $ withSimplify s
+
+withoutSimplify s = runAlex s $ fmap reverse calc
+
+withSimplify s = runAlex s $ fmap (fmap simplify . reverse) calc
--- ex3/app/ParserData.hs
+++ ex3/app/ParserData.hs
@@ -1,9 +1,7 @@
 -- | Module where it contains the Parser data, as it will need GADTS
-module ParserData (Exp (..)) where
+module ParserData (Exp (..), simplify) where
 
 import AlexUserState
-import qualified Data.Map.Strict as Map
-import Lens.Micro
 import Lexer
 
 type Val = Either Double Int
@@ -28,9 +26,50 @@ instance Show Exp where
     TBrack exp -> '(' : show exp ++ ")"
     TVal x -> show x
 
+data WasExp
+  = WasSum
+  | WasMinus
+  | WasMult
+  | WasDiv
+  | WasMod
+  | WasVal
+  | WasNone
+  | WasBrack
+  | WasModOrDivRight
+  deriving (Read, Eq, Ord, Show, Enum)
+
+simplify :: Exp -> Exp
+simplify = simplify' WasNone . removeRedundant
+
+getWasExp :: Exp -> WasExp
+getWasExp e = case e of
+  TSum _ _ -> WasSum
+  TMinus _ _ -> WasMinus
+  TMult _ _ -> WasMult
+  TDiv _ _ -> WasDiv
+  TMod _ _ -> WasMod
+  TVal _ -> WasVal
+  TBrack _ -> WasBrack
+
 -- | Simplifies the expression removing unnecessary brackets
 -- ===Example
--- >>> simplify $ TBrack (TVal 3)
+-- >>> simplify' $ TBrack (TVal 3)
 -- TVal 3
-simplify :: Exp -> Exp
-simplify = undefined
+simplify' :: WasExp -> Exp -> Exp
+simplify' w x = case x of
+  TMinus exp exp' -> TMinus (simplify' WasSum exp) $ simplify' WasMult exp'
+  TMult exp exp' -> TMult (simplify' WasMult exp) $ simplify' WasMult exp'
+  TSum exp exp' -> TSum (simplify' WasSum exp) $ simplify' WasSum exp'
+  TDiv exp exp' -> TDiv (simplify' WasMult exp) $ simplify' WasModOrDivRight exp'
+  TMod exp exp' -> TMod (simplify' WasMult exp) $ simplify' WasModOrDivRight exp'
+  TBrack exp -> case removeRedundant exp of
+    TVal x -> TVal x
+    exp -> let exp' = simplify' WasBrack exp; w' = getWasExp exp' in
+      if w == WasNone || w' >= w
+      then exp'
+      else TBrack exp'
+  TVal x -> TVal x
+
+removeRedundant :: Exp -> Exp
+removeRedundant (TBrack e) = removeRedundant e
+removeRedundant x = x
