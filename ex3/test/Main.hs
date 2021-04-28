160b08f332e4c152e6b7a96a41e10f95e2dfb507
Quim Picó Mora <quimpm99@gmail.com>
2021-04-28 17:08:25 +0200

    some tests

--- ex3/test/Main.hs
+++ ex3/test/Main.hs
@@ -7,13 +7,15 @@ import Parser
 import qualified System.Exit as Exit
 import Control.Monad (when)
 
-testSimplify s s' = TestLabel s (withSimplify s ~=? withoutSimplify s')
+testSimplify s s' = TestLabel s (withoutSimplify s' ~=? withSimplify s)
 
+--Ficar bastants 
 tests = TestList
   [ testSimplify "((2))" "2"
   , testSimplify "(2 * 3) + 2" "2*3+2"
   , testSimplify "(2 * 3) div (4 div 2)" "2 * 3 div (4 div 2)"
   , testSimplify "(2 * 3) mod (4 mod 2)" "2 * 3 mod (4 mod 2)"
+  , testSimplify "((((2+(2*3)) * 3) mod (4 mod 2)*3))" "(2+2*3) * 3 mod (4 mod 2) * 3"
   ]
 
 
